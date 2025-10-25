0e06b270ccc96bb51724033900b0141af7dc4317
paolo <paolo@foo.com>
2025-10-25 15:11:50 +0200

    BLINK LED_BUILDIN

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#ifndef LED_BUILTIN
+    #define LED_PIN PC13
+#else
+    #define LED_PIN LED_BUILTIN
+#endif
+
+
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH); // su molte board PC13 è active-low -> HIGH = LED spento
+}
+
+void loop() {
+    digitalWrite(LED_PIN, LOW);  // LED acceso
+    delay(500);
+    digitalWrite(LED_PIN, HIGH); // LED spento
+    delay(500);
+}
